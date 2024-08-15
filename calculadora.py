ed3985700a4f89ee1be4f38a101bcc670885c76a
wandressareis <reiswandressa@gmail.com>
2024-08-15 10:55:39 -0400

    Mensagem descritiva sobre as alterações

--- calculadora.py
+++ calculadora.py
@@ -3,31 +3,52 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+from selenium.webdriver.firefox.options import Options
 
 import time
 
 class GoogleMapsCalculator:
   def __init__(self):
+    start_time = time.time()
+    
     self.driver = self._initialize_driver()
-    self.driver.implicitly_wait(2)
+    #self.driver.implicitly_wait(2)
+    print("start drive: ", time.time() - start_time)
 
   def _initialize_driver(self):
+
     try:
-      # return webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-      # PARA NÃO VISUALIZAR A INTERFACE DO GOOGLE MAPS 
-      options = webdriver.FirefoxOptions()
-      options.add_argument('-headless')
+      # Create Firefox Profile to disable images
+      # Create Firefox Options
+      options = Options()
+      options.headless = True  # Run in headless mode
+      options.add_argument("--headless")
+
+      # Set preferences directly through options
+      options.set_preference("permissions.default.image", 2)  # Disable images
+      options.set_preference("dom.ipc.plugins.enabled.libflashplayer.so", "false")  # Disable Flash
+      options.set_preference("permissions.default.stylesheet", 2)  # Disable CSS
+
+      # Reduce the page load strategy
+      options.page_load_strategy = 'eager'  # 'eager' means DOMContentLoaded
+
+      # Initialize the WebDriver with options
       driver = webdriver.Firefox(options=options)
+
       return driver
     except Exception as erro:
       print("Erro ao inicializar o driver:", erro)
       return None
+      
 
   def tempo_total(self):
+    start_time = time.time()
     try:
       xpath = '//div[@data-trip-index="0"]//div[contains(text(), "min")]'
       wait = WebDriverWait(self.driver, timeout=3)
       elemento_tempo = wait.until(EC.presence_of_element_located((By.XPATH, xpath)))
+      print("tempo_total: ", time.time() - start_time)
       return str(elemento_tempo.text)
     except Exception as erro:
       print("Erro ao obter tempo total: ", erro)
@@ -43,8 +64,11 @@ class GoogleMapsCalculator:
         partida = base['coordenadas']  # Acessando as coordenadas da base do SAMU
         for j, destino in enumerate(qth):
           destino_coord = [float(coord) for coord in destino.split(',')]  # Convertendo as coordenadas do qth para lista de float
-          try:  
+          try: 
+            
+            get_start_time = time.time()
             self.driver.get("https://www.google.com/maps/dir/" + ','.join(map(str, partida)) + "/" + ','.join(map(str, destino_coord)))
+            print("get duration: ", time.time() - get_start_time)
             tempo_par = self.tempo_total()
             if tempo_par is not None:
               pares_tempo_percurso[int(base["id"])] = tempo_par
